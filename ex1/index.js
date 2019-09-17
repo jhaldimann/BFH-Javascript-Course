0bd16e370293e23df60792d14d327f1f7598c35f
Julian <julian@praxishaldimann.ch>
2019-09-17 08:12:47 +0200

    Use single quotes

--- ex1/index.js
+++ ex1/index.js
@@ -1,20 +1,20 @@
-const alphabetString = "a=.-;b=-...;c=-.-.;d=-..;e=.;f=..-;g=--.;h=....;i=..;j=.---;k=-.-;l=.-..;" +
-  "m=--;n=-.;o=---;p=.---.;q=--.-;r=.-.;s=...;t=-;u=..-;v=...-;w=.--;x=-..-;" +
-  "y=-.-;z=--..; =//;.=.-.-.-;,=--..--;?=..--..;!=-.-.--";
+const alphabetString = 'a=.-;b=-...;c=-.-.;d=-..;e=.;f=..-;g=--.;h=....;i=..;j=.---;k=-.-;l=.-..;' +
+  'm=--;n=-.;o=---;p=.---.;q=--.-;r=.-.;s=...;t=-;u=..-;v=...-;w=.--;x=-..-;' +
+  'y=-.-;z=--..; =//;.=.-.-.-;,=--..--;?=..--..;!=-.-.--';
 
 let charToMorseCode = (input) => {
-  let output = alphabetString.match(input+"=[-./]*");
+  let output = alphabetString.match(input+'=[-./]*');
   output = output[0].substring(2);
   return output;
 };
 
 let convertToMorseCode = (input) => {
-  let output = "";
+  let output = '';
   for(let i = 0; i <= input.length; i++) {
     output += charToMorseCode(input.charAt(i))
   }
   return output;
 };
 
-console.log(convertToMorseCode("hello"));
-console.log(charToMorseCode("!"));
+console.log(convertToMorseCode('hello'));
+console.log(charToMorseCode('a'));
